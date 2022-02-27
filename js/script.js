ce936d91bb15ee9db61ed1fd211eee770ac57a51
leah-creator <leah.angelica.bateman@gmail.com>
2022-02-27 00:33:36 -0800

    jquery

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 console.log ("Hello, welcome to my portfolio!")
 
+
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'navtoggle';
-  }
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
 }
